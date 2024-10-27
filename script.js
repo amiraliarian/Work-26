0aa406056abd51e8068d3e81b4bf5516ae68cc9d
amirali <amiraliariyan4444@gmail.com>
2024-10-27 15:26:35 +0330

    alert

---
+++ script.js
@@ -0,0 +1,14 @@
+var num1 = Number(prompt("num1"));
+var num2 = Number(prompt("num2"));
+var num3 = Number(prompt("num3"));
+var num4 = Number(prompt("num4"));
+var num5 = Number(prompt("num5"));
+var num6 = Number(prompt("num6"));
+var num7 = Number(prompt("num7"));
+var num8 = Number(prompt("num8"));
+var num9 = Number(prompt("num9"));
+var num10 = Number(prompt("num10"));
+alert((num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10) / 10);
+
+
+
